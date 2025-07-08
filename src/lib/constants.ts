c600c8c674bc845069b0f3d4a468e46146dcc7c4
kantzmichael <michael@3dcastor.com>
2025-07-08 09:10:54 +0300

    Bring back 5 sec delay

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
 export const CANVAS_SIZE = 400;
 export const POINT_RADIUS = 4;
 export const PAGE_SIZE = 15;
+export const DELAY_DEFAULT = 5000;
--- src/lib/utils/delay.ts
+++ src/lib/utils/delay.ts
@@ -1,9 +1,11 @@
-export const addDelay = (ms: number = 5000) =>
+import { DELAY_DEFAULT } from "../constants";
+
+export const addDelay = (ms: number = DELAY_DEFAULT) =>
   new Promise((resolve) => setTimeout(resolve, ms));
 
 export function withDelay<Args extends unknown[], R>(
   handler: (...args: Args) => Promise<R>,
-  ms: number = 1000
+  ms: number = DELAY_DEFAULT
 ): (...args: Args) => Promise<R> {
   return async (...args: Args) => {
     await addDelay(ms);
